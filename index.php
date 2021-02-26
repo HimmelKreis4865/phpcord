7136e1d85cc514ab8443af5fce15a75c8a554cae
HimmelKreis4865 <51092444+HimmelKreis4865@users.noreply.github.com>
2021-02-26 19:58:53 +0100

    simplified readme & index

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 
-use phpcord\channel\BaseTextChannel;
-use phpcord\command\Command;
 use phpcord\Discord;
 use phpcord\event\EventListener;
 use phpcord\event\message\MessageSendEvent;
-use phpcord\guild\GuildMessage;
 use phpcord\intents\IntentsManager;
 
 require_once __DIR__ . "/src/phpcord/Discord.php";
@@ -24,21 +21,8 @@ $discord->setIntents(IntentsManager::allIntentsSum());
 
 $discord->registerEvents(new class implements EventListener {
 	public function onSend(MessageSendEvent $event) {
-		if ($event->getMessage()->getMember()->isHuman()) $event->getChannel()->send("test");
+		if ($event->getMessage()->getMember()->isHuman()) $event->getChannel()->send("This is the default bot message! You should edit the source code.");
 	}
 });
 
-$discord->enableCommandMap();
-
-$discord->getCommandMap()->register(new class extends Command {
-
-	public function __construct() {
-		parent::__construct("clear");
-	}
-
-	public function execute(BaseTextChannel $channel, GuildMessage $message, array $args): void {
-		
-	}
-});
-
-$discord->login("ODAwMjkzMzM2ODU3ODM3NTY5.YAQBQA.DljzfLFPS6F9q2yRQhuJ4mX2aqA");
+$discord->login("your bot token");
