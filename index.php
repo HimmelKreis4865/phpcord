0282d1dbf923608dc4caf0ceb392e5b7cb2bfe41
HimmelKreis4865 <51092444+HimmelKreis4865@users.noreply.github.com>
2021-08-14 12:46:24 +0200

    refreshed index

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
 
 use phpcord\Discord;
-use phpcord\event\EventListener;
-use phpcord\event\message\MessageSendEvent;
 use phpcord\intents\IntentsManager;
 
-require_once __DIR__ . "/src/phpcord/Discord.php";
-
-$discord = new Discord([ "debugMode" => true ]);
+require_once __DIR__ . DIRECTORY_SEPARATOR . "vendor/autoload.php";
+$discord = new Discord(__DIR__, [ "debug_mode" => true, "file_log" => true ]);
 
 $discord->setIntents(IntentsManager::allIntentsSum());
+$discord->registerEvents(new EventListener());
 
-$discord->registerEvents(new class implements EventListener {
-	public function onSend(MessageSendEvent $event) {
-		if ($event->getMessage()->getMember()->isHuman()) $event->getChannel()->send("This is the default bot message! You should edit the source code.");
-	}
-});
-
-$discord->login("your bot token");
+$discord->login("your token");
